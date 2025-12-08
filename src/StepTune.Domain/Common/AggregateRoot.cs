35bafcbece7eb168963c447b6ced122c710d6d6c
mohsenSohrabi <mohsen.sohrabi.cs@gmail.com>
2025-12-08 10:30:11 +0330

    feat: provide AggregateRoot

--- src/StepTune.Domain/Common/AggregateRoot.cs
+++ src/StepTune.Domain/Common/AggregateRoot.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace StepTune.Domain.Common
+﻿namespace StepTune.Domain.Common
 {
     public abstract class AggregateRoot:BaseEntity
     {
