a396416bf95080305150d256c126502c28f2cb03
mohsenSohrabi <mohsen.sohrabi.cs@gmail.com>
2025-12-08 11:27:19 +0330

    feat: Provide User Entity

--- src/StepTune.Domain/Users/Entities/User.cs
+++ src/StepTune.Domain/Users/Entities/User.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using StepTune.Domain.Common;
+using StepTune.Domain.Users.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StepTune.Domain.Users.Entities
 {
-    public class User
+    public sealed class User : AggregateRoot
     {
+        private User() { }
+        public string Email { get; private set; } = null!;
+        public string PasswordHash { get; private set; } = null!;
+        public UserProfile Profile { get; private set; } = null!;
+        public DateTime CreatedAt { get; private set; }
+
+        public static Result<User> Register(
+            string email,
+            string passwordHash,
+            UserProfile profile
+            )
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return Result.Failure<User>("Email is required");
+            if (string.IsNullOrWhiteSpace(passwordHash))
+                return Result.Failure<User>("Password hash is required");
+
+            var user = new User
+            {
+                Email = email.Trim().ToLower(),
+                PasswordHash = passwordHash,
+                Profile = profile,
+                CreatedAt = DateTime.UtcNow
+            };
+            return Result.Success<User>(user);
+        }
+
+        public Result UpdateProfile(UserProfile newProfile)
+        {
+            Profile = newProfile;
+            return Result.Success();
+        }
     }
 }
